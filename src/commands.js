df9ef7a0511ab46ff629db81173c9213a8599f59
ercardona <ercardona@edrans.com>
2022-01-06 10:51:07 +0100

    list tasks

--- src/commands.js
+++ src/commands.js
@@ -1,12 +1,13 @@
 const { program } = require('commander');
 const { prompt } = require('inquirer');
 const { addTask } = require('./controllers/task.controllers');
+const { listTasks } = require('./controllers/task.controllers');
 
 program
   .version('0.0.1')
   .description('A simple command line tool to manage tasks');
 
-//command: ode src/commands.js save
+//command: node src/commands.js save
 program.command('save').action(async () => {
   const answers = await prompt([
     {
@@ -23,4 +24,10 @@ program.command('save').action(async () => {
   await addTask(answers);
 });
 
+//command: node src/commands.js list
+program.command('list').action(
+  async () => await listTasks(),
+  // const tasks = await Task.find();
+  // console.log(tasks);
+);
 program.parse(process.argv);
--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -7,8 +7,22 @@ const addTask = async (task) => {
   await connection.close();
 };
 
+const listTasks = async () => {
+  const tasks = await Task.find().lean();
+  console.table(
+    tasks.map((task) => ({
+      _id: task._id.toString(),
+      title: task.title,
+      description: task.description,
+    })),
+  );
+  await connection.close();
+  process.exit(0);
+};
+
 module.exports = {
   addTask,
+  listTasks,
   // findTask,
   // updateTask,
   // removeTask,
